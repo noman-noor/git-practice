4c209b795ba1bcfe976b7369a583dd681bd17ed6
asadakhtar23 <asad.akhtar23@hotmail.com>
2016-04-18 17:38:03 +0500

    dsadasd

--- app/src/main/java/com/example/nauman/myapplication/Asad.java
+++ app/src/main/java/com/example/nauman/myapplication/Asad.java
@@ -1,7 +1,4 @@
 package com.example.nauman.myapplication;
 
-/**
- * Created by asad on 4/15/16.
- */
 public class Asad {
 }
--- app/src/main/java/com/example/nauman/myapplication/Asad2.java
+++ app/src/main/java/com/example/nauman/myapplication/Asad2.java
@@ -1,7 +1,5 @@
 package com.example.nauman.myapplication;
 
-/**
- * Created by asad on 4/18/16.
- */
+
 public class Asad2 {
 }
